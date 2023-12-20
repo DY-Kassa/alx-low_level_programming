6a6261324ad8ac636e6be3131fa71ee59765b11b
David Yolom Kassa <dfkyolom18@outlook.com>
2023-12-20 04:07:10 +0100

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test if int is  - or +.
+ * @i: test value
+ *
+ * Return: 0(always)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
