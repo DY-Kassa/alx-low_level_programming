8d158ac00cd38cc665157003d268f85a0705aba9
David Yolom Kassa <dfkyolom18@gmail.com>
2024-02-10 12:56:43 -0800

    betty proof

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,9 @@
 
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = 59;
+
 	/**
 	 * length of the message -1
 	 * message written to stdout
@@ -18,5 +19,5 @@ int main(void)
 
 	write(2, message, len);
 
-	return(1);
+	return (1);
 }
